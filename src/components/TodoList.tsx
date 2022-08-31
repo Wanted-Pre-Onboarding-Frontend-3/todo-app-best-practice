91620dde4cece9231e0571a1f19c5353ea1dfa72
ycs1m1yk <satoly4@gmail.com>
2022-08-31 23:11:59 +0900

    refactor: Remove unused lines #11
    - axios interceptors 적용 후
    컴포넌트에서 headers에 Authorization 추가하는 코드 삭제

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,103 +1,86 @@
-import React, { useState } from 'react';
-import { Api } from '../api/api';
-import axios, { AxiosError } from 'axios';
-import { getToken } from '../utils/storage';
-import { todos } from '../page/todos';
-import styled from 'styled-components';
-import { colors } from '../core/colors';
-import { CheckIcon } from './icon/check';
-import { Text } from './text';
-import { TrashIcon } from './icon/trash';
-import { Modal } from './Modal';
-import { useForm } from 'react-hook-form';
-
+import React, { useState } from 'react'
+import { Api } from '../api/api'
+import axios, { AxiosError } from 'axios'
+import { getToken } from '../utils/storage'
+import { todos } from '../page/todos'
+import styled from 'styled-components'
+import { colors } from '../core/colors'
+import { CheckIcon } from './icon/check'
+import { Text } from './text'
+import { TrashIcon } from './icon/trash'
+import { Modal } from './Modal'
+import { useForm } from 'react-hook-form'
 
 interface TodoListProps {
-  children: React.ReactNode;
-  id: number;
-  todo: string;
-  isCompleted: boolean;
-  userId: number;
-  setTodo: (StatusType: todos[]) => void;
+  children: React.ReactNode
+  id: number
+  todo: string
+  isCompleted: boolean
+  userId: number
+  setTodo: (StatusType: todos[]) => void
 }
 
 export const TodoList: React.FC<TodoListProps> = (props) => {
-  const { isCompleted, todo, id, setTodo } = props;
-  const { register, watch, reset } = useForm();
-  const [isComposing, setIsComposing] = useState<boolean>(false);
-  const [modalOpen, setModalOpen] = useState<boolean>(false);
-  const token = getToken();
+  const { isCompleted, todo, id, setTodo } = props
+  const { register, watch, reset } = useForm()
+  const [isComposing, setIsComposing] = useState<boolean>(false)
+  const [modalOpen, setModalOpen] = useState<boolean>(false)
+  const token = getToken()
 
   const updateOneTodos = async (args: Pick<TodoListProps, 'isCompleted' | 'todo' | 'id'>) => {
-    const { isCompleted, todo, id } = args;
+    const { isCompleted, todo, id } = args
     const body = {
       todo,
       isCompleted,
-    };
+    }
     try {
-      await Api.updateOneTodo.request(body, id, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+      await Api.updateOneTodo.request(body, id)
 
-      const res = await Api.getManyTodo.request({
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      setTodo(res.data);
+      const res = await Api.getManyTodo.request()
+      setTodo(res.data)
     } catch (error) {
       if (axios.isAxiosError(error)) {
-        const axiosError = error as AxiosError;
-        const { response } = axiosError;
+        const axiosError = error as AxiosError
+        const { response } = axiosError
 
         if (response?.status === 404) {
-          const { data } = response;
+          const { data } = response
           // @ts-ignore
-          alert(data?.message);
+          alert(data?.message)
         }
       }
     }
-  };
+  }
 
   const deleteOneTodos = (id: number) => {
     return async () => {
       try {
-        await Api.deleteOneTodo.request(id, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
+        await Api.deleteOneTodo.request(id)
 
-        const res = await Api.getManyTodo.request({
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        setTodo(res.data);
+        const res = await Api.getManyTodo.request()
+        setTodo(res.data)
       } catch (error) {
         if (axios.isAxiosError(error)) {
-          const axiosError = error as AxiosError;
-          const { response } = axiosError;
+          const axiosError = error as AxiosError
+          const { response } = axiosError
 
           if (response?.status === 404) {
-            const { data } = response;
+            const { data } = response
             // @ts-ignore
-            alert(data?.message);
+            alert(data?.message)
           }
         }
       }
-    };
-  };
+    }
+  }
 
   const onKeyDown: React.KeyboardEventHandler<HTMLInputElement> | KeyboardEvent = async (e) => {
-    if (isComposing) return;
+    if (isComposing) return
     if (e.key === 'Enter') {
-      e.preventDefault();
-      await updateOneTodos({ isCompleted, todo: watch(`todo${id}`), id });
+      e.preventDefault()
+      await updateOneTodos({ isCompleted, todo: watch(`todo${id}`), id })
     }
-  };
+  }
 
   return (
     <DivWrap>
@@ -105,19 +88,26 @@ export const TodoList: React.FC<TodoListProps> = (props) => {
         <IconButton onClick={() => updateOneTodos({ isCompleted: !isCompleted, todo, id })}>
           <CheckIcon size={18} fillColor={isCompleted ? `${colors.primary500}` : `${colors.grey500}`} />
         </IconButton>
-        <Text color={colors.primary700} fontSize='M3'>
+        <Text color={colors.primary700} fontSize="M3">
           {todo}
         </Text>
       </FlexDivWrap>
-      {modalOpen && <Modal closeModal={() => setModalOpen(prev => !prev)}>
-        <InputWrap {...register(`todo${id}`)} defaultValue={todo} onKeyDown={(e) => onKeyDown(e)}
-                   onCompositionStart={() => setIsComposing(true)}
-                   onCompositionEnd={() => setIsComposing(false)} />
-        <ModalModifyButton
-          onClick={() => updateOneTodos({ isCompleted, todo: watch(`todo${id}`), id })}>수정하기</ModalModifyButton>
-      </Modal>}
-      <ModifyButton onClick={() => setModalOpen(prev => !prev)}>
-        <Text color={colors.white} fontSize='S4'>
+      {modalOpen && (
+        <Modal closeModal={() => setModalOpen((prev) => !prev)}>
+          <InputWrap
+            {...register(`todo${id}`)}
+            defaultValue={todo}
+            onKeyDown={(e) => onKeyDown(e)}
+            onCompositionStart={() => setIsComposing(true)}
+            onCompositionEnd={() => setIsComposing(false)}
+          />
+          <ModalModifyButton onClick={() => updateOneTodos({ isCompleted, todo: watch(`todo${id}`), id })}>
+            수정하기
+          </ModalModifyButton>
+        </Modal>
+      )}
+      <ModifyButton onClick={() => setModalOpen((prev) => !prev)}>
+        <Text color={colors.white} fontSize="S4">
           수정
         </Text>
       </ModifyButton>
@@ -125,9 +115,8 @@ export const TodoList: React.FC<TodoListProps> = (props) => {
         <TrashIcon size={15} />
       </IconButton>
     </DivWrap>
-  );
-};
-
+  )
+}
 
 const DivWrap = styled.div`
   display: flex;
@@ -140,30 +129,27 @@ const DivWrap = styled.div`
     display: flex;
     border-bottom: 1px solid ${colors.grey500};
   }
-`;
+`
 
 const InputWrap = styled.input`
   width: 100%;
   border: 1px solid ${colors.grey700};
   height: 40px;
   border-radius: 3px;
-`;
+`
 
 const FlexDivWrap = styled.div`
   display: flex;
   flex: 1;
   align-items: center;
   margin: 0 8px;
-
-
-`;
+`
 
 const ModifyButton = styled.button`
   background: ${colors.grey600};
   cursor: pointer;
   margin: 0 8px;
-`;
-
+`
 
 const ModalModifyButton = styled.button`
   height: 40px;
@@ -173,11 +159,10 @@ const ModalModifyButton = styled.button`
   width: 100%;
 
   margin-top: 8px;
-`;
+`
 
 const IconButton = styled.button`
   background: unset;
   cursor: pointer;
   margin: 0 8px;
-`;
-
+`
