fadd3a7bf51207f87b98616e09e7252ff50c4da8
dev.jun28 <hsj9360@gmail.com>
2022-09-01 00:57:23 +0900

    chore(router): change sign dir

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Main } from '@/page/main'
-import { SignUp } from '@/page/sign-up'
-import { SignIn } from '@/page/sign-in'
-import { Todos } from '@/page/todos'
-import PrivateRoute from '@/components/PrivateRoute'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Main } from '@/page/main';
+import { SignIn, SignUp } from '@/page/sign';
+import { Todos } from '@/page/todos';
+import PrivateRoute from '@/components/PrivateRoute';
 
 export const Router = () => {
   return (
