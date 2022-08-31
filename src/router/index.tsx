7516d203606634c03c1e94d06f323d0d029b0ad8
Lihu Kim <lh.ines.kim@gmail.com>
2022-08-31 23:43:10 +0900

    refactor: import 방식 절대경로로 설정

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Main } from '../page/main'
-import { SignUp } from '../page/sign-up'
-import { SignIn } from '../page/sign-in'
-import { Todos } from '../page/todos'
+import { Main } from '@/page/main'
+import { SignUp } from '@/page/sign-up'
+import { SignIn } from '@/page/sign-in'
+import { Todos } from '@/page/todos'
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         {/*<Route path="/auth" element={<Login />} />*/}
-        <Route path='/' element={<Main />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/signin' element={<SignIn />} />
-        <Route path='/todos' element={<Todos />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/todos" element={<Todos />} />
       </Routes>
     </BrowserRouter>
   )
