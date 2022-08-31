ebea6aa4687b02a7624d7ab1a8acda2c05d740b0
owen <cockyblind@gmail.com>
2022-09-01 00:41:52 +0900

    Revert "Refactor: base url env로 변경 #19"
    This reverts commit c9fb24b9e9107a19afde42edaf9ea05b3f0df626.

--- src/api/axios-instance.ts
+++ src/api/axios-instance.ts
@@ -1,8 +1,10 @@
 import axios from 'axios'
+import { PROPERTIES } from '@/config/properties'
 import { getToken } from '@/utils/storage'
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  //! dotenv lib 사용시 추후 env로 변경
+  baseURL: PROPERTIES.BASE_URL,
 })
 
 axiosInstance.interceptors.request.use(function (config: any) {
