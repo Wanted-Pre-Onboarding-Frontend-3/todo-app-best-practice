2cc99271d99c5d278c68b1697a766082d3f1238d
woowsnu <woowsnu@gmail.com>
2022-09-01 00:27:12 +0900

    refactor: redirect 방식 변경 #10

---
+++ src/components/PrivateRoute.tsx
@@ -0,0 +1,14 @@
+import { Outlet, Navigate, useLocation } from 'react-router-dom';
+import { getToken } from '../utils/storage';
+
+const PrivateRoute = () => {
+    const token = getToken();
+    const { pathname } = useLocation();
+
+    if (pathname === '/todos') {
+        return token ? <Outlet /> : <Navigate to='/' />;
+      }
+      return token ? <Navigate to='/todos' /> : <Outlet />;
+    };
+  
+  export default PrivateRoute;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,16 +4,18 @@ import { Main } from '@/page/main'
 import { SignUp } from '@/page/sign-up'
 import { SignIn } from '@/page/sign-in'
 import { Todos } from '@/page/todos'
+import PrivateRoute from '@/components/PrivateRoute'
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/*<Route path="/auth" element={<Login />} />*/}
-        <Route path="/" element={<Main />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/todos" element={<Todos />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/" element={<Main />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/todos" element={<Todos />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
